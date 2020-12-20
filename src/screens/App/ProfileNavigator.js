68f4174b7bd5785ff96b5fa280d29eada0589793
codefreak13 <senatorcuba.com>
2020-12-20 06:15:40 +0100

    save

--- src/screens/App/ProfileNavigator.js
+++ src/screens/App/ProfileNavigator.js
@@ -1,5 +1,5 @@
-import 'react-native-gesture-handler';
 /* eslint-disable global-require */
+import 'react-native-gesture-handler';
 import React from 'react';
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
